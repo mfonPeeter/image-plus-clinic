01e7d267dfbf5d83231d8f97ed4be4f4f3794824
mfonPeeter <105684251+mfonPeeter@users.noreply.github.com>
2023-04-11 23:04:01 +0100

    fix: corrected some words in Summary.jsx

--- src/components/HomePage/Summary/Summary.jsx
+++ src/components/HomePage/Summary/Summary.jsx
@@ -5,18 +5,18 @@ const Summary = () => {
     <section className="mb-16 px-6 sm:px-12 md:px-20 lg:px-16 xl:px-28">
       <div className="flex flex-col items-center mb-10">
         <h2 className="mb-3 text-center text-3xl text-[#333] font-bold uppercase md:text-4xl">
-          Eye Clinic in Uyo, Akwa Ibom
+          Eye Clinic in Uyo, Port Harcourt
         </h2>
         <div className="w-10 h-[3px] bg-[#f97729]" />
       </div>
       <div className="mb-12">
         <p className="text-xl">
-          ImagePlus is an eye clinic in Akwa Ibom. We specialize in caring for
-          all your vision needs. We handle everything from eye exams,
-          preventative care, treatment, hard-to-fit contact lenses, computer
-          vision problems, specialty lenses to low vision and blindness
-          rehabilitation. We are focused on helping you see throughout your
-          lifetime.
+          ImagePlus is an eye clinic in Akwa Ibom and Port Harcourt. We
+          specialize in caring for all your vision needs. We handle everything
+          from eye exams, preventative care, treatment, hard-to-fit contact
+          lenses, computer vision problems, specialty lenses to low vision and
+          blindness rehabilitation. We are focused on helping you see throughout
+          your lifetime.
         </p>
       </div>
 
