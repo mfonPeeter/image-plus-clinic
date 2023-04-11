82ca4f4ede7d1dcfc38631b17971ab1e285acbcb
mfonPeeter <105684251+mfonPeeter@users.noreply.github.com>
2023-04-12 00:14:09 +0100

    fixed padding and border bug in Services component

--- src/components/HomePage/Services.jsx
+++ src/components/HomePage/Services.jsx
@@ -1,6 +1,6 @@
 const Services = () => {
   return (
-    <section className="mb-16 pt-10 pb-14 text-white bg-[#2f2f2f] md:px-20 lg:px-16 xl:px-28">
+    <section className="mb-16 pt-10 pb-8 text-white bg-[#2f2f2f] md:pb-14 md:px-20 lg:px-16 xl:px-28">
       <div className="flex flex-col items-center mb-12">
         <h2 className="mb-3 text-center text-3xl font-bold uppercase md:text-4xl">
           Our Services
@@ -9,7 +9,7 @@ const Services = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-        <div className="group text-center py-6 pr-4 border-b md:border-r">
+        <div className="group text-center py-6 px-4 border-b md:pr-4 md:border-r">
           <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
             Low Vision Clinic
           </h2>
@@ -38,7 +38,7 @@ const Services = () => {
             Read more
           </a>
         </div>
-        <div className="group text-center py-6 pl-4 border-b lg:px-4 lg:border-r">
+        <div className="group text-center py-6 px-4 border-b md:pl-4 lg:px-4 lg:border-r">
           <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
             Vision Screening
           </h2>
@@ -53,7 +53,7 @@ const Services = () => {
             Read more
           </a>
         </div>
-        <div className="group text-center py-6 pr-4 border-b md:border-r lg:pr-0 lg:pl-4 lg:border-r-0">
+        <div className="group text-center py-6 px-4 border-b md:pr-4 md:border-r lg:pr-0 lg:pl-4 lg:border-r-0">
           <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
             Diagnostic Testing
           </h2>
@@ -83,7 +83,7 @@ const Services = () => {
             Read more
           </a>
         </div>
-        <div className="group text-center py-6 pl-4 border-b lg:px-4 lg:border-b-0 lg:border-r">
+        <div className="group text-center py-6 px-4 border-b md:pl-4 lg:px-4 lg:border-b-0 lg:border-r">
           <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
             Contact Lenses
           </h2>
@@ -98,7 +98,7 @@ const Services = () => {
             Read more
           </a>
         </div>
-        <div className="group text-center py-6 pr-4 border-b md:border-r md:border-b-0 lg:px-4">
+        <div className="group text-center py-6 px-4 md:pr-4 md:border-r md:border-b-0 lg:px-4">
           <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
             The Outdoor Clinic
           </h2>
