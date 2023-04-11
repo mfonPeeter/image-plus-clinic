1f0b89d51aebc1718d5b35491607d95022406262
mfonPeeter <105684251+mfonPeeter@users.noreply.github.com>
2023-04-12 00:04:39 +0100

    add hover effect on the heading text in the services component

--- src/components/HomePage/Services.jsx
+++ src/components/HomePage/Services.jsx
@@ -9,8 +9,10 @@ const Services = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-        <div className="text-center py-6 pr-4  border-b md:border-r">
-          <h2 className="mb-1 text-xl md:text-2xl">Low Vision Clinic</h2>
+        <div className="group text-center py-6 pr-4 border-b md:border-r">
+          <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
+            Low Vision Clinic
+          </h2>
           <p className="text-sm mb-3 md:text-base">
             We create a world without limits by improving the vision for
             near-blind patients.
@@ -22,8 +24,8 @@ const Services = () => {
             Read more
           </a>
         </div>
-        <div className="text-center py-6 px-4 w-full border-b md:border-r">
-          <h2 className="mb-1 text-xl md:text-2xl">
+        <div className="group text-center py-6 px-4 w-full border-b md:border-r">
+          <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
             Geriatric & Pedriatic Care
           </h2>
           <p className="text-sm mb-3 md:text-base">
@@ -36,8 +38,10 @@ const Services = () => {
             Read more
           </a>
         </div>
-        <div className="text-center py-6 pl-4 border-b lg:px-4 lg:border-r">
-          <h2 className="mb-1 text-xl md:text-2xl">Vision Screening</h2>
+        <div className="group text-center py-6 pl-4 border-b lg:px-4 lg:border-r">
+          <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
+            Vision Screening
+          </h2>
           <p className="text-sm mb-3 md:text-base">
             We provide vision screening services to individuals and corporate
             organizations.
@@ -49,8 +53,10 @@ const Services = () => {
             Read more
           </a>
         </div>
-        <div className="text-center py-6 pr-4 border-b md:border-r lg:pr-0 lg:pl-4 lg:border-r-0">
-          <h2 className="mb-1 text-xl md:text-2xl">Diagnostic Testing</h2>
+        <div className="group text-center py-6 pr-4 border-b md:border-r lg:pr-0 lg:pl-4 lg:border-r-0">
+          <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
+            Diagnostic Testing
+          </h2>
           <p className="text-sm mb-3 md:text-base">
             We utilize computerized diagnostic equipment in the management of
             our patients.
@@ -62,8 +68,10 @@ const Services = () => {
             Read more
           </a>
         </div>
-        <div className="text-center py-6 px-4 border-b md:border-r lg:px-0 lg:pr-4 lg:border-b-0">
-          <h2 className="mb-1 text-xl md:text-2xl">Optical Services</h2>
+        <div className="group text-center py-6 px-4 border-b md:border-r lg:px-0 lg:pr-4 lg:border-b-0">
+          <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
+            Optical Services
+          </h2>
           <p className="text-sm mb-3 md:text-base">
             We offer a wide range of Optical services including single vision,
             bifocal and varifocal lenses.
@@ -75,8 +83,10 @@ const Services = () => {
             Read more
           </a>
         </div>
-        <div className="text-center py-6 pl-4 border-b lg:px-4 lg:border-b-0 lg:border-r">
-          <h2 className="mb-1 text-xl md:text-2xl">Contact Lenses</h2>
+        <div className="group text-center py-6 pl-4 border-b lg:px-4 lg:border-b-0 lg:border-r">
+          <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
+            Contact Lenses
+          </h2>
           <p className="text-sm mb-3 md:text-base">
             Count on our knowledge and expertise to find the contact lenses
             perfect for your needs.
@@ -88,8 +98,10 @@ const Services = () => {
             Read more
           </a>
         </div>
-        <div className="text-center py-6 pr-4 border-b md:border-r md:border-b-0 lg:px-4">
-          <h2 className="mb-1 text-xl md:text-2xl">The Outdoor Clinic</h2>
+        <div className="group text-center py-6 pr-4 border-b md:border-r md:border-b-0 lg:px-4">
+          <h2 className="mb-1 text-xl transition-colors group-hover:text-[#f97729] md:text-2xl">
+            The Outdoor Clinic
+          </h2>
           <p className="text-sm mb-3 md:text-base">
             High quality and professional service you can trust, in the comfort
             of your home.
