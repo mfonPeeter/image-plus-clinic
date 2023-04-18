36c7cc584f6ec6d7996e1c8086a4a1364631f20d
mfonPeeter <105684251+mfonPeeter@users.noreply.github.com>
2023-04-18 14:16:18 +0100

    fix: reduced image width on mobile view to fix side scrolling

--- src/components/HomePage/Partners.jsx
+++ src/components/HomePage/Partners.jsx
@@ -4,7 +4,7 @@ import moniepointLogo from "../../assets/partners/moniepoint-logo.png";
 
 const Partners = () => {
   return (
-    <section className="pt-10 pb-4 px-6 bg-[#efefef] text-white sm:px-12 md:px-20 lg:px-16 xl:px-28">
+    <section className="pt-10 pb-4 bg-[#efefef] text-white sm:px-12 md:px-20 lg:px-16 xl:px-28">
       <div className="flex flex-col items-center mb-2">
         <h2 className="mb-3 text-center text-3xl text-[#333] font-bold uppercase md:text-4xl">
           Our Partners
@@ -12,9 +12,9 @@ const Partners = () => {
         <div className="w-10 h-[3px] bg-[#f97729]" />
       </div>
       <div className="flex justify-center space-x-1 sm:space-x-10">
-        <img src={ubaLogo} alt="UBA Logo" />
-        <img src={keystoneLogo} alt="Keystone Logo" />
-        <img src={moniepointLogo} alt="Moniepoint Logo" />
+        <img className="w-[126px]" src={ubaLogo} alt="UBA Logo" />
+        <img className="w-[126px]" src={keystoneLogo} alt="Keystone Logo" />
+        <img className="w-[126px]" src={moniepointLogo} alt="Moniepoint Logo" />
       </div>
     </section>
   );
