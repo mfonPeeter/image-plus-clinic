ff8cb121a7a9c0d67be1b968e1fe9c3eded27f02
mfonPeeter <105684251+mfonPeeter@users.noreply.github.com>
2023-04-20 00:13:40 +0100

    installed react-responsive-carousel yarn package in order to implement the carousel functionality in the hero section

--- src/components/HomePage/Hero.jsx
+++ src/components/HomePage/Hero.jsx
@@ -1,42 +1,72 @@
-import larrIcon from "../../assets/hero/larr.svg";
-import rarrIcon from "../../assets/hero/rarr.svg";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const HeroSection = () => {
   return (
-    <header className="relative flex justify-center bg-heroBg-sm-1 bg-cover bg-no-repeat bg-top mb-16 md:justify-end md:mb-20 md:bg-heroBg-1g-1">
-      <div className="py-32 px-16 text-white md:py-56">
-        <div className="text-center">
-          <h2 className="mb-3 font-bold text-3xl sm:text-4xl md:text-[55px]">
-            We Are Imageplus Clinic
-          </h2>
-          <p className="mb-10 text-xl italic sm:text-2xl md:text-3xl">
-            Your Primary Eye Care Practitioners
-          </p>
-        </div>
-        <div className="flex flex-col space-y-3 md:flex-row md:space-x-4 md:space-y-0 md:justify-center">
-          <button className="py-5 px-4 uppercase font-semibold bg-[rgba(0,0,0,0.36)] rounded transition-all hover:bg-black sm:px-10">
-            Meet Our Doctors
-          </button>
-          <button className="py-5 px-4 uppercase font-semibold bg-[rgba(0,0,0,0.36)] rounded transition-all hover:bg-black sm:px-10">
-            Schedule Appointment
-          </button>
+    <Carousel>
+      <div className="relative flex justify-center bg-heroBg-sm-1 bg-cover bg-no-repeat bg-top h-full mb-16 md:justify-end md:mb-20 md:bg-heroBg-1g-1">
+        <div className="py-32 px-16 text-white md:py-56">
+          <div className="text-center">
+            <h2 className="mb-3 font-bold text-3xl sm:text-4xl md:text-[55px]">
+              We Are Imageplus Clinic
+            </h2>
+            <p className="mb-10 text-xl italic sm:text-2xl md:text-3xl">
+              Your Primary Eye Care Practitioners
+            </p>
+          </div>
+          <div className="flex flex-col space-y-3 md:flex-row md:space-x-4 md:space-y-0 md:justify-center">
+            <button className="py-5 px-4 uppercase font-semibold bg-[rgba(0,0,0,0.36)] rounded transition-all hover:bg-black sm:px-10">
+              Meet Our Doctors
+            </button>
+            <button className="py-5 px-4 uppercase font-semibold bg-[rgba(0,0,0,0.36)] rounded transition-all hover:bg-black sm:px-10">
+              Schedule Appointment
+            </button>
+          </div>
         </div>
       </div>
-      <button className="absolute top-1/2 left-0 -translate-y-1/2 md:left-4">
-        <img
-          className="w-12 h-12 md:w-14 md:h-14"
-          src={larrIcon}
-          alt="Left Arrow"
-        />
-      </button>
-      <button className="absolute top-1/2 right-0 -translate-y-1/2 md:right-4">
-        <img
-          className="w-12 h-12 md:w-14 md:h-14"
-          src={rarrIcon}
-          alt="Right Arrow"
-        />
-      </button>
-    </header>
+
+      <div className="relative flex justify-center bg-heroBg-sm-2 bg-cover bg-no-repeat bg-top h-full mb-16 md:justify-end md:mb-20 md:bg-heroBg-1g-2">
+        <div className="py-32 px-16 text-white md:py-56">
+          <div className="text-center">
+            <h2 className="mb-3 font-bold text-3xl sm:text-4xl md:text-[55px]">
+              We Are Imageplus Clinic
+            </h2>
+            <p className="mb-10 text-xl italic sm:text-2xl md:text-3xl">
+              Your Primary Eye Care Practitioners
+            </p>
+          </div>
+          <div className="flex flex-col space-y-3 md:flex-row md:space-x-4 md:space-y-0 md:justify-center">
+            <button className="py-5 px-4 uppercase font-semibold bg-[rgba(0,0,0,0.36)] rounded transition-all hover:bg-black sm:px-10">
+              Meet Our Doctors
+            </button>
+            <button className="py-5 px-4 uppercase font-semibold bg-[rgba(0,0,0,0.36)] rounded transition-all hover:bg-black sm:px-10">
+              Schedule Appointment
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative flex justify-center bg-heroBg-sm-3 bg-cover bg-no-repeat bg-top h-full mb-16 md:justify-end md:mb-20 md:bg-heroBg-1g-3">
+        <div className="py-32 px-16 text-white md:py-56">
+          <div className="text-center">
+            <h2 className="mb-3 font-bold text-3xl sm:text-4xl md:text-[55px]">
+              We Are Imageplus Clinic
+            </h2>
+            <p className="mb-10 text-xl italic sm:text-2xl md:text-3xl">
+              Your Primary Eye Care Practitioners
+            </p>
+          </div>
+          <div className="flex flex-col space-y-3 md:flex-row md:space-x-4 md:space-y-0 md:justify-center">
+            <button className="py-5 px-4 uppercase font-semibold bg-[rgba(0,0,0,0.36)] rounded transition-all hover:bg-black sm:px-10">
+              Meet Our Doctors
+            </button>
+            <button className="py-5 px-4 uppercase font-semibold bg-[rgba(0,0,0,0.36)] rounded transition-all hover:bg-black sm:px-10">
+              Schedule Appointment
+            </button>
+          </div>
+        </div>
+      </div>
+    </Carousel>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       backgroundImage: {
         "heroBg-1g-1": "url('/src/assets/sliders/slider-one-lg.jpg')",
         "heroBg-sm-1": "url('/src/assets/sliders/slider-one-sm.jpg')",
+        "heroBg-1g-2": "url('/src/assets/sliders/slider-two-lg.jpg')",
+        "heroBg-sm-2": "url('/src/assets/sliders/slider-two-sm.jpg')",
+        "heroBg-1g-3": "url('/src/assets/sliders/slider-three-lg.jpg')",
+        "heroBg-sm-3": "url('/src/assets/sliders/slider-three-sm.jpg')",
         "eye-clinic": "url('/src/assets/summary/eye-clinic.jpg')",
         services: "url('/src/assets/summary/services.jpg')",
         "visit-us": "url('/src/assets/summary/visit-us.jpg')",
