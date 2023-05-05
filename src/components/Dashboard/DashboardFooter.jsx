80a2b27f99e4605cb2277693e74d97f124d3d518
mfonPeeter <105684251+mfonPeeter@users.noreply.github.com>
2023-05-05 17:16:49 +0100

    made DashboardFooter to be fully responsive

--- src/components/Dashboard/DashboardFooter.jsx
+++ src/components/Dashboard/DashboardFooter.jsx
@@ -1,6 +1,6 @@
 const DashboardFooter = () => {
   return (
-    <div className="flex items-center justify-center py-4">
+    <div className="flex items-center justify-center py-4 text-sm sm:text-base">
       <p>Copyright &copy; Imageplus | by Dealdrive Technology</p>
     </div>
   );
